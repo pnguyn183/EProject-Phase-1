36ab0793017f68f2f5c994cf57c30758b0725132
pnguyn183 <binmaster0987@gmail.com~>
2025-10-21 23:42:49 +0700

    Xong docker

--- auth/src/middlewares/authMiddleware.js
+++ auth/src/middlewares/authMiddleware.js
@@ -6,17 +6,17 @@ const config = require("../config");
  */
 
 module.exports = function(req, res, next) {
-  const token = req.header("x-auth-token");
+    const token = req.header("x-auth-token");
 
-  if (!token) {
-    return res.status(401).json({ message: "No token, authorization denied" });
-  }
+    if (!token) {
+        return res.status(401).json({ message: "No token, authorization denied" });
+    }
 
-  try {
-    const decoded = jwt.verify(token, config.jwtSecret);
-    req.user = decoded;
-    next();
-  } catch (e) {
-    res.status(400).json({ message: "Token is not valid" });
-  }
+    try {
+        const decoded = jwt.verify(token, config.jwtSecret);
+        req.user = decoded;
+        next();
+    } catch (e) {
+        res.status(400).json({ message: "Token is not valid" });
+    }
 };
--- order/src/utils/messageBroker.js
+++ order/src/utils/messageBroker.js
@@ -3,30 +3,30 @@ const config = require("../config");
 const OrderService = require("../services/orderService");
 
 class MessageBroker {
-  static async connect() {
-    try {
-      const connection = await amqp.connect(config.rabbitMQUrl);
-      const channel = await connection.createChannel();
-
-      // Declare the order queue
-      await channel.assertQueue(config.rabbitMQQueue, { durable: true });
-
-      // Consume messages from the order queue on buy
-      channel.consume(config.rabbitMQQueue, async (message) => {
+    static async connect() {
         try {
-          const order = JSON.parse(message.content.toString());
-          const orderService = new OrderService();
-          await orderService.createOrder(order);
-          channel.ack(message);
+            const connection = await amqp.connect(config.rabbitMQUrl);
+            const channel = await connection.createChannel();
+
+            // Declare the order queue
+            await channel.assertQueue(config.rabbitMQQueue, { durable: true });
+
+            // Consume messages from the order queue on buy
+            channel.consume(config.rabbitMQQueue, async(message) => {
+                try {
+                    const order = JSON.parse(message.content.toString());
+                    const orderService = new OrderService();
+                    await orderService.createOrder(order);
+                    channel.ack(message);
+                } catch (error) {
+                    console.error(error);
+                    channel.reject(message, false);
+                }
+            });
         } catch (error) {
-          console.error(error);
-          channel.reject(message, false);
+            console.error(error);
         }
-      });
-    } catch (error) {
-      console.error(error);
     }
-  }
 }
 
 module.exports = MessageBroker;
